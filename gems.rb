59b78af39a426bf763c94596e3c0999c9b9395d0
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-08 11:35:00 +0000

    Add rubocop and fix offenses

--- gems.rb
+++ gems.rb
@@ -1,27 +1,21 @@
 source 'https://rubygems.org'
 
-# TODO : remove?
-ruby '2.5.3'
-
-gem 'rails', '~> 5.2.2'
+gem 'coffee-rails', '~> 4.2'
+gem 'enumerize'
+gem 'jbuilder', '~> 2.5'
 gem 'mysql2', '>= 0.4.4', '< 0.6.0'
 gem 'puma', '~> 3.11'
-# Use SCSS for stylesheets
+gem 'rails', '~> 5.2.2'
 gem 'sass-rails', '~> 5.0'
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
-
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
-gem 'turbolinks', '~> 5'
-gem 'jbuilder', '~> 2.5'
 gem 'sidekiq'
+gem 'turbolinks', '~> 5'
+gem 'uglifier', '>= 1.3.0'
 gem 'wicked'
 
 group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'capybara', '~> 2.13'
-  gem 'rubocop', '~> 0.42'
+  gem 'rubocop', '~> 0.62.0'
   gem 'selenium-webdriver'
   gem 'simplecov'
 end
@@ -30,11 +24,11 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'flamegraph'
-  gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'rack-mini-profiler', '~> 0.10'
   gem 'stackprof'
+  gem 'web-console', '>= 3.3.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
