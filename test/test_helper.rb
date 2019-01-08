d9ada752865073f63fc8d64a70aa8889cd6d93c5
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-08 15:52:22 +0000

    Integrate SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/app/admin/'
+  add_filter '/app/channels/'
+  add_filter '/app/jobs/'
+  add_filter '/app/mailers/'
+  add_filter '/bin/'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
