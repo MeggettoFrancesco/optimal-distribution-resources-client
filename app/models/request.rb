6512766aa29ee916cb4016d3140ad6c5050292b0
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-08 12:24:01 +0000

    Fix offenses

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   extend Enumerize
-  
-  enumerize :request_type, in: %i[input_matrix, open_street_maps]
+
+  enumerize :request_type, in: %i[input_matrix open_street_maps]
   enumerize :algorithm_type, in: %i[greedy]
 
   validates :request_type, presence: true
