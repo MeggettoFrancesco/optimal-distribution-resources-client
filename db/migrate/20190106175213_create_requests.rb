2145c2cf7d5278fd4011914a44c0229808210c0f
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-08 11:28:55 +0000

    Add request_type and algorithm_type to Request

--- db/migrate/20190106175213_create_requests.rb
+++ db/migrate/20190106175213_create_requests.rb
@@ -2,6 +2,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
       t.string :request_type
+      t.string :algorithm_type
       
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_06_175213) do
+
+  create_table "requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "request_type"
+    t.string "algorithm_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
