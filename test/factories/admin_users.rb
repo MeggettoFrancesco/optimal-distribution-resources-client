de7773075e1505a853422bacad7fc530192f3114
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-08 15:39:34 +0000

    Update Factory for AdminUsers

--- test/factories/admin_users.rb
+++ test/factories/admin_users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :admin_user do
-    
+    email { Faker::Internet.safe_email }
+    password 'password'
+    password_confirmation 'password'
+    website { Website.all.try(:sample) || FactoryBot.build(:website) }
   end
 end
