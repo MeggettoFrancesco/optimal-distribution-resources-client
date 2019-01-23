f9d25d79b24f4ed53684872583bb83784bd81a28
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-23 17:39:42 +0000

    Add Sidekiq admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  require 'sidekiq/web'
+  authenticate :admin_user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   resources :requests, only: %i[index new create]
   root 'requests#index'
 end
