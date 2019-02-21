11075ddcdf2eea55d14d05e58c014a1b937834ed
Francesco Meggetto <fra_megg@hotmail.com>
2019-02-21 15:15:15 +0100

    Add OSM xml file uploader

---
+++ app/uploaders/open_street_map_xml_uploader.rb
@@ -0,0 +1,11 @@
+class OpenStreetMapXmlUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w[osm]
+  end
+end
