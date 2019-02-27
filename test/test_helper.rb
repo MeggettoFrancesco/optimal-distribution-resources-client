5ad4435b2778a2cbd7a86b14486b0578e1c1f5c7
Francesco Meggetto <fra_megg@hotmail.com>
2019-02-27 16:36:52 +0100

    Add WebMock gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'webmock/minitest'
+WebMock.disable_net_connect!
+
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
 end
