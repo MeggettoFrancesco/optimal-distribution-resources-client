70170d60cf552011f531ee00e2e40b2fd94c99c1
Francesco Meggetto <fra_megg@hotmail.com>
2019-03-01 20:07:21 +0100

    Fix controller test, specify request_type

--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -12,7 +12,7 @@ class RequestsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'requests#create should redirect to requests#show' do
-    my_request = build(:request)
+    my_request = build(:request, request_type: :input_matrix_request)
     stub_osm_api(my_request)
 
     post requests_path(
@@ -21,7 +21,6 @@ class RequestsControllerTest < ActionDispatch::IntegrationTest
 
     new_request = Request.last
     assert_equal my_request.odr_api_matrix, new_request.odr_api_matrix
-
     assert_redirected_to request_path(id: new_request.id)
   end
 
